e3b4b1cc76541a70c203e46e8fc98791c8ccf099
nyamzy <john.nyamweya@student.moringaschool.com>
2021-07-26 22:53:23 +0300

    modify the service to fetch data

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -1,13 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../user'; 
+import { Observable } from 'rxjs';
+import { IApiResponse } from 'src/app/users'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  getUser(){
-    return User
+  
+  constructor(private http:HttpClient) { }
+
+  getUser(userName):Observable<IApiResponse[]>{
+    return this.http.get<IApiResponse[]>("https://api.github.com/users/"+userName)
   }
-  constructor() { }
 }
