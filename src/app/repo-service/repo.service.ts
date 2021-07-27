1fb41f977eb37a487f6fdd7572d1612f9bba1e2c
nyamzy <john.nyamweya@student.moringaschool.com>
2021-07-27 20:33:29 +0300

    write function for getting repos details

--- src/app/repo-service/repo.service.ts
+++ src/app/repo-service/repo.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { IApiResponse } from "src/app/users";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getUser(userName):Observable<IApiResponse[]>{
+    return this.http.get<IApiResponse[]>("https://api.github.com/users/"+userName+"/repos?acces_token=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")
+  }
 }
+
